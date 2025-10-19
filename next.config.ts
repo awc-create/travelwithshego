2f5ec5232df8ea2fe6d28f3808751c588eef8d10
Adnan 2025 <adaptiveworkflowconsultancy@gmail.com>
2025-10-19 15:26:16 +0100

    first vercel commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'utfs.io' }, // UploadThing CDN
+      { protocol: 'https', hostname: 'uploadthing.com' },
+      { protocol: 'https', hostname: 'cdn.uploadthing.com' },
+      // add any other external hosts you use for images
+    ],
+    // If you really want to bypass  optimization locally:
+    // unoptimized: true,
+  },
+  // If you’re deploying to a Node server:
+  // output: 'standalone',
 };
 
-export default nextConfig;
+module.exports = nextConfig;
